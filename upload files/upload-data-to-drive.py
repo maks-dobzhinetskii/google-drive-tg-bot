50489ccdab8358c99bcf9bcadaffdbd0f0c95016
Макс Добжинецький Добжинецький <maks.dobzhinetsky14@gmail.com>
2022-11-15 14:14:45 +0200

    Check for existence

--- upload files/upload-data-to-drive.py
+++ upload files/upload-data-to-drive.py
@@ -15,27 +15,34 @@ file_list = file_list.fillna('')
 
 
 def upload_files() -> None:
-    for row in file_list.iterrows():
-        file_name = row[1]['File Name']
-        parent_folder_id = row[1]['Fold er Id']
-        starred = row[1]['Starred']
-        file_path = row[1]['File Path']
+    folder_id = '1lVZH7ZUeYa2Seat-UYFz99TLkrVMa0VQ'
+    query = f"parents = '{folder_id}'"
+    response = service.files().list(q=query).execute()
+    files = response.get('files')
 
-        mime_type, _ = mimetypes.guess_type(file_path)
-        media_content = MediaFileUpload(file_path)
+    if not files:
+        for row in file_list.iterrows():
+            file_name = row[1]['File Name']
+            parent_folder_id = row[1]['Folder Id']
+            starred = row[1]['Starred']
+            file_path = row[1]['File Path']
 
-        request_body = {
-            'name': file_name,
-            'starred': starred
-        }
+            mime_type, _ = mimetypes.guess_type(file_path)
+            media_content = MediaFileUpload(file_path, mimetype=mime_type)
 
-        if parent_folder_id:
-            request_body['parents'] = [parent_folder_id]
+            request_body = {
+                'name': file_name,
+                'starred': starred
+            }
 
-        service.files().create(
-            body=request_body,
-            media_body=media_content
-        ).execute()
+            if parent_folder_id:
+                request_body['parents'] = [parent_folder_id]
+
+            service.files().create(
+                body=request_body,
+                media_body=media_content
+            ).execute()
+    
 
 
 if __name__ == '__main__':
