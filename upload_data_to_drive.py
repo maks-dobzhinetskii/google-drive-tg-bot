849127534dbcc3fbb3270e3dfc3140dedd95c195
Макс Добжинецький Добжинецький <maks.dobzhinetsky14@gmail.com>
2022-11-17 16:23:22 +0200

    Updated a file checker

--- upload_data_to_drive.py
+++ upload_data_to_drive.py
@@ -21,29 +21,28 @@ def upload_files() -> None:
     response = service.files().list(q=query).execute()
     files = response.get('files')
 
-    if not files:
-        for row in file_list.iterrows():
-            file_name = row[1]['File Name']
-            parent_folder_id = row[1]['Folder Id']
-            starred = row[1]['Starred']
-            file_path = row[1]['File Path']
+    for row in file_list.iterrows():
+        file_name = row[1]['File Name']
+        parent_folder_id = row[1]['Folder Id']
+        starred = row[1]['Starred']
+        file_path = row[1]['File Path']
 
-            mime_type, _ = mimetypes.guess_type(file_path)
-            media_content = MediaFileUpload(file_path, mimetype=mime_type)
+        mime_type, _ = mimetypes.guess_type(file_path)
+        media_content = MediaFileUpload(file_path, mimetype=mime_type)
 
-            request_body = {
-                'name': file_name,
-                'starred': starred
-            }
+        request_body = {
+            'name': file_name,
+            'starred': starred
+        }
 
-            if parent_folder_id:
-                request_body['parents'] = [parent_folder_id]
-
+        if parent_folder_id:
+            request_body['parents'] = [parent_folder_id]
+        
+        if not any(file_name == dictionary['name'] for dictionary in files):
             service.files().create(
                 body=request_body,
                 media_body=media_content
-            ).execute()
-    
+            ).execute()   
 
 
 if __name__ == '__main__':
