a3c1ea4af982aca17f21e24cf11bec2833d87604
Макс Добжинецький <maksdobzinetskii@Makss-MacBook-Pro.local>
2022-11-29 15:12:42 +0200

    Created orm using sqlalchemy

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.dialects.mysql import INTEGER
 
 
@@ -7,10 +7,26 @@ Base = declarative_base()
 metadata = Base.metadata
 
 
-class UserInfo(Base):
+class Folder(Base):
+    __tablename__ = 'folder'
 
+    id = Column(INTEGER(20), primary_key=True)
+    name_folder = Column(String(255))
+    id_drive_folder = Column(String(255))
+
+
+class Message(Base):
+    __tablename__ = 'message'
+
+    id = Column(INTEGER(20), primary_key=True)
+    media_group_id = Column(INTEGER)
+    file_name = Column(String(50))
+    folder_id = Column(INTEGER, ForeignKey('folder.id'))
+
+
+class UserInfo(Base):
     __tablename__ = 'userinfo'
 
-    id = Column(INTEGER(11), primary_key=True)
+    id = Column(INTEGER(20), primary_key=True)
     username = Column(String(255))
-    role = Column(String(50))
+    message_id = Column(INTEGER, ForeignKey('message.id'))
